de9adda75bc3449f92675bc4f6e781f318cfe8d6
Lexirieru <axelurwawuskaatarubby@mail.ugm.ac.id>
2026-02-18 02:21:16 +0700

    chore: Update lending pool addresses in contracts configuration.

--- lib/contracts.ts
+++ lib/contracts.ts
@@ -3,8 +3,8 @@ import { arbitrumSepolia } from "wagmi/chains";
 export const CHAIN = arbitrumSepolia;
 
 export const LENDING_POOL_ADDRESSES: readonly `0x${string}`[] = [
-  "0x126a3c7bf5b143e8cfd6cf1e1610f3cc021c10ca",
-  "0x1C1f7C86174023C9D53c9d6aBD76d117891a5738",
+  "0xa234207393c1e8345f7d8428f3f9cd3ccef18365",
+  "0x76604928143775545b613499fad77dff0da5d601",
 ] as const;
 
 export const MULTICALL_ADDRESS = "0xe9a1adc452cd26cae2062d997a97a3800eaaeaa3" as const;
